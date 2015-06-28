f98e5a807067a778812c97140361baffb82fe54b
Bong Kwon <bongkwon@Bongs-MacBook-Pro.local>
2015-06-28 16:01:37 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('qr_pass');
+    collection.find({},{},function(e,docs){
+		res.setHeader('Content-Type', 'application/json');
+    	res.send(JSON.stringify(docs));
+    });
+});
+
 module.exports = router;
+
+
+router.get('/ukc/:ukcId/:date/:meal', function(req, res) {
+	 var db = req.db;
+    var collection = db.get('qr_pass');
+    var query = {"ukc-id" : req.param("ukcId"), "date" : req.param("date"), "meal" : req.param("meal")}
+    collection.find(query,{},function(e,docs){
+
+
+		res.setHeader('Content-Type', 'application/json');
+    	res.send(JSON.stringify(docs));
+    });
+
+
+    setTimeout(function() {
+		collection.update(query, {$set: {"type": "NOK"}}, function(err) {
+		if (err) throw err
+		});
+	}, 3000);
+
+
+});
